dae5c1d5908e97baf01f351bd46114353b4b208d
peaceful-state-9936 <efua.mensahbrown@gmail.com>
2019-04-12 01:26:35 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -9,7 +9,7 @@ function addElementToBeginningOfArray( ){
 
 function destructivelyAddElementToBeginningOfArray(){
  var myArray = [1];
-  myArray.unshift( =  )"foo"
+  myArray.unshift("foo")
   return myArray
 }
 
